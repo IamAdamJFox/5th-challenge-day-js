42381de279566f17667c6f86c1d1eef6b6d590ec
adamjfox <adamjeffersonfox@gmail.com>
2023-07-06 02:44:28 -0700

    wrong thing

--- assests/script.js
+++ assests/script.js
@@ -1,5 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
+
+
+
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -14,7 +17,14 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+const today = dayjs();
+const currentHour = parseInt(today.format("H"))
+
+$(".time-block").each(function () {
+  const timeBlock = $(this);
+  const hour = timeBlock.attr("id").split("-").pop();
+  console.log(hour);
+});
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
